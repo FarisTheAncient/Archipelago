fe51753266b4078be8007180c1dc67f61a91453a
qwint <qwint.42@gmail.com>
2024-07-10 14:37:49 -0500

    same fix as for Text Client in 3633, I do think there are other issues with UT --nogui though

--- worlds/tracker/__init__.py
+++ worlds/tracker/__init__.py
@@ -4,7 +4,10 @@ from worlds.LauncherComponents import Component, components, Type, launch_subpro
 
 def launch_client():
     from .TrackerClient import launch as TCMain
-    launch_subprocess(TCMain, name="Universal Tracker client")
+    if not sys.stdout or "--nogui" not in sys.argv:
+        launch_subprocess(TCMain, name="Universal Tracker client")
+    else:
+        TCMain()
 
 class TrackerWorld:
     pass
