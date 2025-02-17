52d821db79a9863b5d6c986374f7c9efdb2281ee
Bastien Orivel <eijebong@bananium.fr>
2025-02-17 16:12:19 +0100

    Ignore aliases on the safe dumper, not the regular one
    f34fde3f318aa284224ce945b94acad0e1ddcb11 overrode the normal dumper
    which isn't used as we use `safe_dump`.

--- worlds/fuzzer/fuzz.py
+++ worlds/fuzzer/fuzz.py
@@ -79,7 +79,7 @@ def world_from_apworld_name(apworld_name):
 
 
 # See https://github.com/yaml/pyyaml/issues/103
-yaml.Dumper.ignore_aliases = lambda *args: True
+yaml.SafeDumper.ignore_aliases = lambda *args: True
 
 # Adapted from archipelago'd generate_yaml_templates
 # https://github.com/ArchipelagoMW/Archipelago/blob/f75a1ae1174fb467e5c5bd5568d7de3c806d5b1c/Options.py#L1504
