244ef8a30ad8e0d488263e792082b077aaf64f8e
Seph De Busser <seph.debusser@gmail.com>
2025-12-07 15:32:06 +0100

    Remove assumption that map and icon keys are different
    If the map index and icon key are the same, this results in the icon coordinates callback not being called when the map stays the same because of an early exit in load_map.

--- worlds/tracker/TrackerClient.py
+++ worlds/tracker/TrackerClient.py
@@ -1300,7 +1300,7 @@ class TrackerGameContext(CommonContext):
                         if args["key"] == key:
                             self.load_map(None)
                             self.updateTracker()
-                        elif args["key"] == icon_key:
+                        if args["key"] == icon_key:
                             self.update_location_icon_coords()
                     elif "keys" in args:
                         if icon_key in args["keys"]:
